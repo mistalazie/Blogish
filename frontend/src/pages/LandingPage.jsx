cc1fb089061f03777f536b0847c12425e785cc7c
mistalazie <kiransai537@gmail.com>
2024-05-18 18:33:16 +0530

    Top bar added

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,9 @@
 export function LandingPage () {
     return (
-        <div>Landing Page</div>
+        <div className="flex bg-yellow-100 w-full h-screen">
+            <div className="m-40 h-[80vh] w-[180vh] bg-yellow-500">
+                hi
+            </div>
+        </div>
     )
 }
